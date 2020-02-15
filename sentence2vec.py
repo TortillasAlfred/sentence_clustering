c026ef550f09fe5d491c6b0594e977ba53953041
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2020-02-15 15:46:50 -0500

    Initial commit

---
+++ sentence2vec.py
@@ -0,0 +1,122 @@
+#!/usr/bin/python3
+
+#
+#  Copyright 2016-2018 Peter de Vocht
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import numpy as np
+from sklearn.decomposition import PCA
+from typing import List
+
+# see spacy_sentence2vec.py for an example usage with real language inputs
+
+
+# an embedding word with associated vector
+class Word:
+    def __init__(self, text, vector):
+        self.text = text
+        self.vector = vector
+
+    def __str__(self):
+        return self.text + ' : ' + str(self.vector)
+
+    def __repr__(self):
+        return self.__str__()
+
+
+# a sentence, a list of words
+class Sentence:
+    def __init__(self, word_list):
+        self.word_list = word_list
+
+    # return the length of a sentence
+    def len(self) -> int:
+        return len(self.word_list)
+
+    def __str__(self):
+        word_str_list = [word.text for word in self.word_list]
+        return ' '.join(word_str_list)
+
+    def __repr__(self):
+        return self.__str__()
+
+
+# todo: get a proper word frequency for a word in a document set
+# or perhaps just a typical frequency for a word from Google's n-grams
+def get_word_frequency(word_text):
+    return 0.0001  # set to a low occurring frequency - probably not unrealistic for most words, improves vector values
+
+
+# A SIMPLE BUT TOUGH TO BEAT BASELINE FOR SENTENCE EMBEDDINGS
+# Sanjeev Arora, Yingyu Liang, Tengyu Ma
+# Princeton University
+# convert a list of sentence with word2vec items into a set of sentence vectors
+def sentence_to_vec(sentence_list: List[Sentence],
+                    embedding_size: int,
+                    vocab,
+                    a: float = 1e-3):
+    num_tokens = sum(vocab.freqs.values())
+
+    sentence_set = []
+    for sentence in sentence_list:
+        # add all word2vec values into one vector for the sentence
+        vs = np.zeros(embedding_size)
+        sentence_length = sentence.len()
+        for word in sentence.word_list:
+            # smooth inverse frequency, SIF
+            a_value = a / (a + vocab.freqs[word.text] / num_tokens)
+            # vs += sif * word_vector
+            vs = np.add(vs, np.multiply(a_value, word.vector))
+
+        vs = np.divide(vs, sentence_length)  # weighted average
+        # add to our existing re-calculated set of sentences
+        sentence_set.append(vs)
+
+    # calculate PCA of this sentence set
+    pca = PCA()
+    pca.fit(np.array(sentence_set))
+    u = pca.components_[0]  # the PCA vector
+    u = np.multiply(u, np.transpose(u))  # u x uT
+
+    # pad the vector?  (occurs if we have less sentences than embeddings_size)
+    if len(u) < embedding_size:
+        for i in range(embedding_size - len(u)):
+            # add needed extension for multiplication below
+            u = np.append(u, 0)
+
+    # resulting sentence vectors, vs = vs -u x uT x vs
+    sentence_vecs = []
+    for vs in sentence_set:
+        sub = np.multiply(u, vs)
+        sentence_vecs.append(np.subtract(vs, sub))
+
+    return sentence_vecs
+
+
+def sentence2vec(sentences, vocab):
+    # convert the above sentences to vectors using spacy's large model vectors
+    sentence_list = []
+    for sentence in sentences:
+        word_list = []
+        for word in sentence[1]:
+            word_list.append(Word(word, vocab.vectors[vocab.stoi[word]]))
+        if len(word_list) > 0:  # did we find any words (not an empty set)
+            sentence_list.append(Sentence(word_list))
+
+    # apply single sentence word embedding
+    embedding_size = vocab.vectors.shape[1]
+    sentence_vectors = sentence_to_vec(sentence_list, embedding_size,
+                                       vocab)  # all vectors converted together
+
+    return sentence_vectors
---
+++ sentence_clustering.py
@@ -0,0 +1,293 @@
+import csv
+
+from torchtext.vocab import Vocab
+from collections import Counter, OrderedDict, defaultdict
+from itertools import product
+import os
+import pickle
+import shutil
+from nltk.corpus import stopwords
+from sentence2vec import sentence2vec
+from copy import deepcopy
+from sklearn.cluster import KMeans, SpectralClustering, DBSCAN
+from sklearn.metrics import silhouette_score
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import numpy as np
+from scipy.spatial.distance import cdist
+from joblib import Parallel, delayed
+from gensim.models.fasttext import load_facebook_model
+
+
+def load_all_csv_rows(file_path):
+    with open(file_path, encoding='utf8') as csvfile:
+        reader = csv.reader(csvfile)
+        all_rows = []
+        for row in reader:
+            all_rows.append(row[0])
+
+    return all_rows
+
+
+def load_all_domains():
+    return load_all_csv_rows('Domain-mobility-test-RA1.csv')
+
+
+def load_all_items():
+    return load_all_csv_rows('Mobility_item_test_RA.csv')
+
+
+def create_folder_for_config(config, base_path):
+    save_path = base_path
+
+    for key, val in config.items():
+        save_path = os.path.join(save_path, f'{key}::{val}')
+
+    if os.path.isdir(save_path):
+        shutil.rmtree(save_path)
+
+    os.makedirs(save_path)
+
+    return save_path
+
+
+def get_vocab_counter(sents, word_filtering):
+    raw_sents = deepcopy(sents)
+    sents = [sent.replace('"', '') for sent in sents]
+    sents = [sent.replace(',', ' ') for sent in sents]
+    text = text.replace('/', ' / ')
+    text = text.replace('.-', ' .- ')
+    text = text.replace('.', ' . ')
+    text = text.replace('\'', ' \' ')
+    text = text.lower()
+    sents = [sent.split() for sent in sents]
+
+    if word_filtering is 'none':
+        filtering = lambda _: True
+    elif word_filtering is 'stopwords':
+        stopwords = set(stopwords.words('english'))
+        filtering = lambda word: word not in stopwords
+    elif word_filtering is 'len3':
+        filtering = lambda word: len(word) > 3
+    else:
+        raise ValueError('Incorrect word filtering')
+
+    sents = [list(filter(filtering, sent)) for sent in sents]
+    sents = [(raw_sents[i], sent) for i, sent in enumerate(sents)
+             if len(sent) > 0]
+    words = [word for sent in sents for word in sent[1]]
+
+    return Counter(words), sents
+
+
+def preprocess(sents, word_filtering, vectors):
+    vocab_counter, sents = get_vocab_counter(sents, word_filtering)
+
+    if isinstance(vectors, int):
+        vocab = Vocab(vocab_counter,
+                      vectors=f'glove.6B.{vectors}d',
+                      specials=[])
+        vocab.vectors = vocab.vectors.numpy()
+
+        # unk_idx = np.nonzero(
+        #     vocab.vectors.max(axis=1) == vocab.vectors.min(axis=1))[0]
+
+        # if len(unk_idx) > 0:
+        #     unk_words = [vocab.itos[idx] for idx in unk_idx]
+        #     raise ValueError(
+        #         f'Some words were not found in the embeddings list ! They are \n\n{unk_words}\n'
+        #     )
+    elif vectors is 'bio':
+        vectors = load_facebook_model(
+            '.vector_cache/BioWordVec_PubMed_MIMICIII_d200.bin')
+        vectors.save_word2vec_format('.vector_cache/biomed_vecs')
+
+    return vocab, sents
+
+
+def get_clustering_obj(method, clusters):
+    if method == 'kmeans':
+        return KMeans(n_clusters=clusters,
+                      random_state=42,
+                      n_init=20,
+                      n_jobs=-1)
+    elif method == 'spectral':
+        return SpectralClustering(n_clusters=clusters,
+                                  random_state=42,
+                                  n_init=20,
+                                  n_jobs=-1)
+    elif method == 'dbscan':
+        return DBSCAN(eps=2.0 / (clusters - 3), n_jobs=-1)
+    else:
+        raise ValueError('Unknown clustering method')
+
+
+def run_clustering(method, clusters, sentences, sentence_vectors, base_path):
+    clustering_obj = get_clustering_obj(method, clusters)
+
+    labels = clustering_obj.fit_predict(sentence_vectors)
+
+    score = silhouette_score(sentence_vectors, labels, metric='cosine')
+
+    return score, labels
+
+
+def get_rows(sentences, sentence_vectors, labels):
+    class_vectors = defaultdict(list)
+    class_sentences = defaultdict(list)
+
+    for sentence, vector, label in zip(sentences, sentence_vectors, labels):
+        class_vectors[label].append(vector)
+        class_sentences[label].append(sentence)
+
+    classes = {}
+
+    for label, vectors in class_vectors.items():
+        vectors = np.asarray(vectors)
+        center = np.mean(vectors, axis=0)
+        dists = cdist(vectors, np.expand_s(center, axis=0), metric='cosine')
+        sorted_args = np.argsort(dists[:, 0])
+        classes[label] = [
+            class_sentences[label][idx][0] for idx in sorted_args
+        ]
+
+    classes = list(
+        reversed(sorted(classes.values(), key=lambda item: len(item))))
+
+    return classes
+
+
+def save_results(sentences, sentence_vectors, labels, save_path):
+    # PLOT CLUSTERS
+    reduced_vectors = PCA(n_components=2).fit_transform(sentence_vectors)
+
+    plt.figure()
+    plt.scatter(reduced_vectors[:, 0], reduced_vectors[:, 1], c=labels)
+    plt.xticks()
+    plt.yticks()
+    plt.savefig(os.path.join(save_path, 'pca_clusters.png'))
+    plt.close()
+
+    reduced_vectors = TSNE(n_components=2,
+                           n_jobs=-1).fit_transform(sentence_vectors)
+
+    plt.figure()
+    plt.scatter(reduced_vectors[:, 0], reduced_vectors[:, 1], c=labels)
+    plt.xticks()
+    plt.yticks()
+    plt.savefig(os.path.join(save_path, 'tsne_clusters.png'))
+    plt.close()
+
+    # SAVE RESULTS TO CSV
+    with open(os.path.join(save_path, 'clusters.csv'), 'w') as f:
+        writer = csv.writer(f)
+
+        rows_data = get_rows(sentences, sentence_vectors, labels)
+        n_rows = len(rows_data[0])
+
+        for i in range(n_rows):
+            row = []
+
+            for column in rows_data:
+                if i < len(column):
+                    row.append(column[i])
+
+            writer.writerow(row)
+
+
+def sentence_vectorize(method, sents, vocab):
+    if method == 'baseline':
+        return sentence2vec(sents, vocab)
+    elif method == 'sent2vec':
+        raise NotImplementedError()
+    else:
+        raise NotImplementedError(
+            f'{method} is not a valid sentence vectorizer.')
+
+
+@delayed
+def launch_from_config(config, base_path, sents):
+    save_path = create_folder_for_config(config, base_path)
+
+    vocab, sents = preprocess(sents, config['word_filtering'],
+                              config['vectors'])
+    sent_embeddings = sentence_vectorize(config['sent_embedder'], sents, vocab)
+
+    score, labels = run_clustering(config['method'], config['clusters'], sents,
+                                   sent_embeddings, base_path)
+
+    save_results(sents, sent_embeddings, labels, save_path)
+
+    return (config, score)
+
+
+def domains_clustering():
+    domains = load_all_domains()
+
+    results_dir = './results/domains'
+    if not os.path.isdir(results_dir):
+        os.makedirs(results_dir)
+
+    results = OrderedDict()
+    hparams = OrderedDict()
+
+    hparams['clusters'] = list(range(4, 9))
+    hparams['word_filtering'] = ['none', 'stopwords', 'len3']
+    hparams['vectors'] = ['bio', 50, 100, 200, 300]
+    hparams['method'] = ['kmeans']
+    hparams['sent_embedder'] = ['sent2vec', 'baseline']
+
+    all_configs = product(*[[(key, val) for val in vals]
+                            for key, vals in hparams.items()])
+    all_configs = tqdm([OrderedDict(config) for config in all_configs])
+
+    results = Parallel(n_jobs=1)(
+        launch_from_config(config, results_dir, domains)
+        for config in all_configs)
+
+    results = sorted(results, key=lambda item: item[1])
+
+    for result in results:
+        print(result)
+
+    with open(os.path.join(results_dir, 'results.pck'), 'wb') as f:
+        pickle.dump(results, f)
+
+
+def items_clustering():
+    items = load_all_items()
+
+    results_dir = './results/items'
+    if not os.path.isdir(results_dir):
+        os.makedirs(results_dir)
+
+    results = OrderedDict()
+    hparams = OrderedDict()
+
+    hparams['clusters'] = list(range(4, 9))
+    hparams['word_filtering'] = ['none', 'stopwords', 'len3']
+    hparams['vectors'] = ['bio', 50, 100, 200, 300]
+    hparams['method'] = ['kmeans']
+    hparams['sent_embedder'] = ['sent2vec', 'baseline']
+
+    all_configs = product(*[[(key, val) for val in vals]
+                            for key, vals in hparams.items()])
+    all_configs = tqdm([OrderedDict(config) for config in all_configs])
+
+    results = Parallel(n_jobs=1)(launch_from_config(config, results_dir, items)
+                                 for config in all_configs)
+
+    results = sorted(results, key=lambda item: item[1])
+
+    for result in results:
+        print(result)
+
+    with open(os.path.join(results_dir, 'results.pck'), 'wb') as f:
+        pickle.dump(results, f)
+
+
+if __name__ == '__main__':
+    items_clustering()
+    domains_clustering()
