84bec4b3109aaa02b4a1c8729f1d1dd66ddfbda4
Mathieu Godbout <magod@beluga1.int.ets1.calculquebec.ca>
2020-09-08 14:08:07 -0400

    run on lulam

--- sentence_clustering.sh
+++ sentence_clustering.sh
@@ -1,8 +1,8 @@
 #!/bin/bash                                      
-#SBATCH --account=def-lulam50                                # Account with resources
-#SBATCH --cpus-per-task=20                                    # Number of CPUs
+#SBATCH --account=def-lulam50                                 # Account with resources
+#SBATCH --cpus-per-task=30                                    # Number of CPUs
 #SBATCH --mem=125G                                            # memory (per node)
-#SBATCH --time=0-03:00                                        # time (DD-HH:MM)
+#SBATCH --time=0-01:00                                        # time (DD-HH:MM)
 #SBATCH --mail-user=mathieu.godbout.3@ulaval.ca               # Where to email
 #SBATCH --mail-type=FAIL                                      # Email when a job fails
 
